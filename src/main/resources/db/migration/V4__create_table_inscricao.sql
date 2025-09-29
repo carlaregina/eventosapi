5cd979bc694e36fe0a5eec75990ac1314e8b42f9
Gabriel Neves <gabriel.santos@capgemini.com>
2025-09-29 18:42:06 -0300

    feat: adiciona migration para criação de tabela de inscrição

---
+++ src/main/resources/db/migration/V4__create_table_inscricao.sql
@@ -0,0 +1,10 @@
+CREATE TABLE inscricao (
+    id_inscricao BIGSERIAL PRIMARY KEY,
+    id_evento BIGINT NOT NULL,
+    id_usuario BIGINT NOT NULL,
+    data TIMESTAMP NOT NULL DEFAULT NOW(),
+    tipo VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_inscricao_evento FOREIGN KEY (id_evento) REFERENCES evento(id_evento),
+    CONSTRAINT fk_inscricao_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+    CONSTRAINT uq_inscricao UNIQUE (id_evento, id_usuario)
+);
