129e6efa2f9dd31813c1ed2b204a2cce95d680dc
carlaregina <carla.reginavasconcellos@gmail.com>
2025-09-29 16:53:11 -0300

    feat: criação do projetos eventosapi

---
+++ src/test/java/com/eventosapi/demo/EventosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventosapi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
