77a50ac00f40acc4c516a7c69275fe02d904cef2
tatianambmorais <tatianambmorais@gmail.com>
2025-09-29 22:28:52 -0300

    adicionando direotiros de domain, dto e model. adicionando entidade inscricao

---
+++ src/main/java/com/eventosapi/demo/domain/dto/InscricaoDTO.java
@@ -0,0 +1,19 @@
+package com.eventosapi.demo.domain.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record InscricaoDTO(
+
+        Long id,
+
+        @NotNull(message = "O id do evento é obrigatório")
+        Long idEvento,
+
+        @NotNull(message = "O id do usuário é obrigatório")
+        Long idUsuario,
+
+        @NotNull(message = "A data é obrigatória")
+        LocalDateTime data
+) {}
