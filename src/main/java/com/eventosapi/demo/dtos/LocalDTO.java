dc5504d0c8ddf25815e88b07c05ae5139152dfbd
Gabriel Neves <gabriel.santos@capgemini.com>
2025-09-30 14:09:33 -0300

    feat: adicionando validators no dto de local

--- src/main/java/com/eventosapi/demo/dtos/LocalDTO.java
+++ src/main/java/com/eventosapi/demo/dtos/LocalDTO.java
@@ -1,16 +1,45 @@
 package com.eventosapi.demo.dtos;
 
+import org.hibernate.validator.constraints.Length;
+
 import com.eventosapi.demo.enums.TipoLocal;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class LocalDTO {
+
+    @NotBlank(message = "O nome é obrigatório")
+    @Length(max = 255, message = "O nome deve ter no máximo 255 caracteres")
     private String nome;
+
+    @NotBlank(message = "O cep é obrigatório")
+    @Pattern(regexp = "\\d{5}-\\d{3}", message = "O cep deve estar no formato 00000-000")
     private String cep;
+
+    @NotBlank(message = "O logradouro é obrigatório")
+    @Length(max = 255, message = "O logradouro deve ter no máximo 255 caracteres")
     private String logradouro;
+
+    @NotBlank(message = "O numero é obrigatório")
+    @Length(max = 255, message = "O numero deve ter no máximo 255 caracteres")
     private String numero;
+
+    @NotBlank(message = "O bairro é obrigatório")
+    @Length(max = 255, message = "O bairro deve ter no máximo 255 caracteres")
     private String bairro;
+
+    @NotBlank(message = "A cidade é obrigatório")
+    @Length(max = 255, message = "A cidade deve ter no máximo 255 caracteres")
     private String cidade;
+
+    @NotBlank(message = "O estado é obrigatório")
+    @Length(max = 2, message = "O estado deve ter no máximo 2 caracteres")
     private String estado;
+
+    @NotNull(message = "O tipo é obrigatório")
     private TipoLocal tipo;
 }
