9593d2e4ecacf3ec1980b3246c2dd55b4cba771a
tatianambmorais <tatianambmorais@gmail.com>
2025-10-07 15:05:13 -0300

    feature: criando microserviço de comunicacoes

---
+++ comunicacoes/src/main/java/com/eventosapi/comunicacoes/ComunicacoesApplication.java
@@ -0,0 +1,13 @@
+package com.eventosapi.comunicacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComunicacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComunicacoesApplication.class, args);
+	}
+
+}
