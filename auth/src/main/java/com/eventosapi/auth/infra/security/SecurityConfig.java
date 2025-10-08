cf4741f0757bd5c16192915de0f4267474aa26fa
Gabriel Neves <gabriel.santos@capgemini.com>
2025-10-08 14:22:24 -0300

    feat: adiciona configuração do swagger

--- auth/src/main/java/com/eventosapi/auth/infra/security/SecurityConfig.java
+++ auth/src/main/java/com/eventosapi/auth/infra/security/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/documentacao/**", "/documentacao.html").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
                 .anyRequest().authenticated()
             ).build();
