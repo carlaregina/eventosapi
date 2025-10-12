f5b48114fb02f61a3ed51e6266e45e43e203ccbd
Gabriel Neves <gabriel.santos@capgemini.com>
2025-10-12 19:22:16 -0300

    feat: configurando prometheus para obter métricas dos módulos

--- local/src/main/java/com/eventosapi/infra/security/SecurityConfig.java
+++ local/src/main/java/com/eventosapi/infra/security/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/documentacao/**", "/documentacao.html").permitAll()
-                .requestMatchers("/actuator/health").permitAll()
+                .requestMatchers("/actuator/**").permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
