1d4a3986d01533f1fae10ada65d06c754340f6b8
Gabriel Neves <gabriel.santos@capgemini.com>
2025-10-14 15:10:14 -0300

    feat: adicionando voucher service

---
+++ comunicacoes/src/main/java/com/eventosapi/comunicacoes/application/services/VoucherService.java
@@ -0,0 +1,55 @@
+package com.eventosapi.comunicacoes.application.services;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.eventosapi.comunicacoes.application.port.EventoClientPort;
+import com.eventosapi.comunicacoes.application.port.UsuarioClientPort;
+import com.eventosapi.comunicacoes.domain.model.Email;
+import com.eventosapi.comunicacoes.domain.model.Evento;
+import com.eventosapi.comunicacoes.domain.model.Inscricao;
+import com.eventosapi.comunicacoes.domain.model.Usuario;
+import com.eventosapi.comunicacoes.interfaces.dto.InscricaoDTO;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class VoucherService {
+    
+    private final PDFService pdfService;
+    private final EmailService emailService;
+    private final EventoClientPort eventoClient;
+    private final UsuarioClientPort usuarioClient;
+
+    public void enviarVoucherInscricao(InscricaoDTO dto) {
+        Inscricao inscricao = from(dto);
+        byte[] pdf = pdfService.geraRelatorioPDF(inscricao);
+        emailService.enviar(Email.inscricaoRealizada(inscricao.getUsuario().getEmail(), Map.of("voucher.pdf", pdf)));
+    }
+
+    public void enviarVoucherAtualizado(InscricaoDTO dto) {
+        Inscricao inscricao = from(dto);
+        byte[] pdf = pdfService.geraRelatorioPDF(inscricao);
+        emailService.enviar(Email.eventoAtualizado(inscricao.getUsuario().getEmail(), Map.of("voucher.pdf", pdf)));
+    }
+
+    private Inscricao from(InscricaoDTO dto) {
+        Evento evento = buscarEventoPorId(dto.getIdEvento());
+        Usuario usuario = buscarUsuarioPorId(dto.getIdUsuario());
+        return new Inscricao(dto.getId(), evento, usuario, dto.getData(), dto.getStatus());
+    }
+
+    private Usuario buscarUsuarioPorId(Long id) {
+        return usuarioClient.findById(id)
+            .orElseThrow(() -> new RuntimeException("Usuário não encontrado com ID: " + id));
+    }
+
+    private Evento buscarEventoPorId(Long id) {
+        return eventoClient.findById(id)
+            .orElseThrow(() -> new RuntimeException("Evento não encontrado com ID: " + id));
+    }
+}
