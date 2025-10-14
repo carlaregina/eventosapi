2dc1cc483a2271e8603a8c5124dc097907f6ab92
Gabriel Neves <gabriel.santos@capgemini.com>
2025-10-13 21:37:36 -0300

    fix: ajustes de merge

--- auth/src/main/java/com/eventosapi/auth/infra/entities/UsuarioEntity.java
+++ auth/src/main/java/com/eventosapi/auth/infra/entities/UsuarioEntity.java
@@ -44,12 +44,10 @@ public class UsuarioEntity implements UserDetails {
     @Column(name = "tipo", length = 255)
     private TipoUsuario tipo;
 
-    
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_" + tipo.name()));
+        return List.of(new SimpleGrantedAuthority(tipo.toString()));
     }
-
 
     @Override
     public String getUsername() {
